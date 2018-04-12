a86c10d46a60aa31ffacb109c4185f002fcd8719
wimpykatana <wim.katana@gmail.com>
2018-04-12 10:25:17 +0700

    update gyro

--- www/js/acclemoter.js
+++ www/js/acclemoter.js
@@ -19,21 +19,73 @@
 
 var app = {
     // Application Constructor
+
+
+
     initialize: function() {
+
+
         document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
 
 
     },
 
     onSuccess: function(acceleration) {
-        // alert('Acceleration X: ' + acceleration.x + '\n' +
-        //     'Acceleration Y: ' + acceleration.y + '\n' +
-        //     'Acceleration Z: ' + acceleration.z + '\n' +
-        //     'Timestamp: '      + acceleration.timestamp + '\n');
+
+        var movement = 10;
+        var accX = Math.round(acceleration.x*10) / 10;
+        var accY = Math.round(acceleration.y*10) / 10;
+
+        var baseX = 100;
+        var baseY = 150;
+
+        var xA = (accX / 10) * movement;
+        var yA = (accY / 10) * movement;
+
+        var acelX = Math.round(acceleration.x);
+        var acelY = Math.round(acceleration.y);
+        var c = document.getElementById('myCanvas');
+        var context = c.getContext('2d');
+        var xPos = 0;
+        var yPos = 0;
+
 
         document.getElementById("accx").innerHTML = 'Sumbu X: ' + Math.round(acceleration.x);
         document.getElementById("accy").innerHTML = 'Sumbu y: ' + Math.round(acceleration.y);
         document.getElementById("accz").innerHTML = 'Sumbu z: ' + Math.round(acceleration.z);
+        document.getElementById("timestamp").innerHTML = 'timestamp: ' + acceleration.timestamp;
+
+
+        document.getElementById("debug").innerHTML = 'Debug: '  + document.getElementById("bola").style.left +' '+ document.getElementById("bola").style.top;
+        document.getElementById("debug2").innerHTML = 'Debug: '  + xA +' '+ yA;
+
+        document.getElementById("bola").style.left = baseX + acelX+'px';
+        document.getElementById("bola").style.top = baseY + acelY+'px';
+
+        // make_base();
+        //
+        // function make_base()
+        // {
+        //     target = new Image();
+        //     target.src = '../crosshair.png';
+        //     target.onload = function(){
+        //         context.drawImage(target, 0, 0);
+        //     }
+        // }
+
+
+        target = new Image();
+        target.src = '../crosshair.png';
+        // target.onload = function(){
+        //     context.drawImage(base_image, 0, 0);
+        // }
+
+
+        context.clearRect(0, 0, 240, 297);
+        xPos += -1*(acelX * 1.5);
+        yPos += (acelY * 1.5);
+        context.drawImage(target, xPos, yPos);
+
     },
 
     onError: function() {
@@ -46,11 +98,9 @@ var app = {
     // Bind any cordova events here. Common events are:
     // 'pause', 'resume', etc.
     onDeviceReady: function() {
-        // console.log("%c KABOOOMMMMM.....",
-        //     "color: rgb(235, 50, 50); " +
-        //     "font-size: 32px; font-family: 'Arial', sans-serif !important; font-weight: bold; font-style:italic");
-        // navigator.accelerometer.getCurrentAcceleration(accelerometerSuccess, accelerometerError);
-        // navigator.accelerometer.getCurrentAcceleration(this.onSuccess.bind(this), this.onError.bind(this));
+
+        this.init();
+
 
         var options = { frequency: 100 };
         navigator.accelerometer.getCurrentAcceleration(this.onSuccess.bind(this), this.onError.bind(this));
@@ -59,6 +109,25 @@ var app = {
         // console.log("wimpy");
 
     },
+
+    init: function () {
+        console.log("hello");
+
+
+        // var c = document.getElementById('myCanvas');
+        // var context = c.getContext('2d');
+        // var xPos = 0;
+        // var yPos = 0;
+        // target = new Image();
+        // target.src = '../crosshair.png';
+        // target.onload = function(){
+        //     context.drawImage(target, 0, 0);
+        // }
+
+
+
+
+    }
 
 };
 
